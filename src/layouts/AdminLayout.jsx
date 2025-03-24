78ebb05c9d81f23988f0112ac68d9363cadf0b75
pragati9991 <juyalpragati9991@gmail.com>
2025-03-24 12:05:35 +0100

    added header/footer

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import RequireAdminAuth from '../components/RequireAdminAuth';
 
 const AdminLayout = () => {
   return (
     <RequireAdminAuth>
-      <Outlet />
+      <>
+        <Header isAdminPage={true} />
+        <Outlet />
+        <Footer />
+      </>
     </RequireAdminAuth>
   );
 };
